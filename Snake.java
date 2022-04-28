c9dfc62f02e6092bd0cbfcc91771a4d9d10d46a4
Manny-op <54683249+Manny-op@users.noreply.github.com>
2022-04-28 10:08:17 -0700

    Base Snake Game Uploaded
    Base template of snake game uploaded but there could be more added

---
+++ Snake.java
@@ -0,0 +1,31 @@
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+
+public class Snake extends JFrame{
+
+    Board game = new Board();
+    public Snake()
+    {
+        initUI();
+    }
+
+    private void initUI()
+    {
+        add(game);
+        setResizable(false);
+        pack();
+
+        setTitle("2nake");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            JFrame ex = new Snake();
+            ex.setVisible(true);
+        });
+    }
+}
