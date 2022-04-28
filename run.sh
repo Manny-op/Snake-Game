7165569a2678b5bbaa5c56c33a28778f71c76de6
Manny-op <54683249+Manny-op@users.noreply.github.com>
2022-04-28 11:42:51 -0700

    fixed the bash script to run

--- run.sh
+++ run.sh
@@ -1,9 +1,16 @@
 echo Remove old byte-code files if any exist
 rm *.class
 
+echo View list of source files
+ls -l *.java
 
-
+echo Compile Board.java
 javac Board.java
+
+echo Compile Snake.java
 javac Snake.java
 
+echo Execute the Payroll Program
 java Snake
+
+echo End of execution. Thank you
